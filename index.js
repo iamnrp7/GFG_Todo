43260aafc5f5430841ac4c2b696b797bf0c216b6
Nihar Prajapati <143262287+iamnrp7@users.noreply.github.com>
2024-02-09 17:36:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://admin:admin@cluster1.nqvqztn.mongodb.net?retryWrites=true&writeConcern=majority");
+
+const app=express();
+app.listen(7001,()=>{
+    console.log("hey server is up and alive");
+});
+
+// middlewares
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//routes
+app.use(require("./routes/index"));
+module.exports=app;
